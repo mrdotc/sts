67727ef4d433d6e1d8af9ffbd8ab4c18d3afa40b
chenxiufeng <chenxiufeng@aspirecn.com>
2019-10-26 11:30:47 +0800

    initial project

---
+++ src/main/java/sts/sts/StsApplication.java
@@ -0,0 +1,18 @@
+package sts.sts;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(StsApplication.class);
+
+	public static void main(String[] args) {
+		LOGGER.debug("Starting StsApplication");
+		SpringApplication.run(StsApplication.class, args);
+		LOGGER.debug("Starting StsApplication");
+	}
+
+}
---
+++ src/test/java/sts/sts/StsApplicationTests.java
@@ -0,0 +1,13 @@
+package sts.sts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
