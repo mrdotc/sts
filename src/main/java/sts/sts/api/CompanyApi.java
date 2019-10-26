fe598f5ef75b7ea424dfbb5592c6fb5238c7ff78
chenxiufeng <chenxiufeng@aspirecn.com>
2019-10-26 16:16:34 +0800

    fix again-three
    # Conflicts:
    #	src/main/java/sts/sts/StsApplication.java
    
    # Conflicts:
    #	src/main/java/sts/sts/StsApplication.java

---
+++ src/main/java/sts/sts/api/CompanyApi.java
@@ -0,0 +1,25 @@
+/*
+ * aspire-tech.com Inc.
+ * Copyright (c) 2000-2019 All Rights Reserved.
+ */
+package sts.sts.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 11
+ *
+ * @author Admin
+ * @version $id:CompanyApi.java, v 11 2019/10/26 14:44:20, Admin Exp $
+ */
+public class CompanyApi {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CompanyApi.class);
+
+    public String getcompanyInfo(String userId) {
+
+        return "Company information";
+
+    }
+}
